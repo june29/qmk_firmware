39b80e39c878f9ae1ff48477a733e45c07505006
june29 <june29.jp@gmail.com>
2018-08-19 16:43:35 +0900

    :sparkles: @june29's original keymap

--- keyboards/ergodox_ez/keymaps/june29/keymap.c
+++ keyboards/ergodox_ez/keymaps/june29/keymap.c
@@ -131,7 +131,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS,
                  KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_MPLY,
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS,
-                          KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS,
+                          KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS,
        BL_TOGG, KC_TRNS,
        BL_ON,
        BL_OFF,  KC_TRNS, KC_WBAK
